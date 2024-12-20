784c0bfae381c4b72b3d92469740348e7d744a08
Max Walter <walt@walt-dev.de>
2024-12-20 12:55:57 +0100

    Added Build

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from './components/ui/button'
 import { RadioGroup, RadioGroupItem } from './components/ui/radio-group'
 import { Label } from './components/ui/label'
 import { Input } from './components/ui/input'
